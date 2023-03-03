3c70790c8cef880a599701dd89c3f0e981bf6e69
Elżbieta Skrzypek <125986679+ElzbietaSkrzypek@users.noreply.github.com>
2023-03-03 12:29:36 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+import pyautogui
+import time
+import numpy as np
+from PIL import Image
+
+time.sleep(3)
+
+# --- Check dinos top of the nose position. Run program and set mose pointer on the dinos top of the nose.
+# Main result: Point(x=189, y=315), so I will chose x=190, y=300 as x and y of region to do screenshot.
+#
+# print(pyautogui.position())
+#
+while True:
+    image = pyautogui.screenshot("screenshot3.png", region=(
+        190, 300, 120, 100))  # 120 and 100 is a weight and height of region which works for me.
+
+    img = np.asarray(Image.open("screenshot3.png"))  # set np array to count pixels on screenshot
+
+    black_pixel_sum = np.sum(img < 100)
+    white_pixel_sum = np.sum(img > 100)
+    print(black_pixel_sum)
+    print(white_pixel_sum)  # - at the beginning it helps me to set the range of light and dark mode
+
+    # light mode - black pixel sum to 30000
+    if 350 < black_pixel_sum < 30000:
+        pyautogui.press('up')
+
+    # dark mode - black pixel over 30000 and white pixels over 1000 works properly for my.
+    # I estimated this range by observation white and dark pixels printed.
+    elif 30000 < black_pixel_sum and white_pixel_sum > 1000:
+        pyautogui.press('up')
